6d901eacf1ccce82fc95b253fe65f254dac67719
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-30 18:53:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,14 @@ def line(array)
   end
 end
 end
+def line(array)
+  line_size = array.size
+  if line_size==0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently:"
+    array.each_with_index do |name,index|
+      print " #{index+1}. #{name}"
+  end
+end
+end
