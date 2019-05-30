b15a94a4887f7bc4234667d0ba57995515e7b6ce
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-30 18:32:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli=[]
+other_deli=["Logan", "Avi", "Spencer"]
+another_deli=["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(array)
+  line_size = array.size
+  if line_size==0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently:" array.each{|name| "#{name}+ #{counter+=1}."}
+  end
+end
